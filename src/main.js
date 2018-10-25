5e14d8f9c8f82daed16287105acc0b74dd589ea0
Hazmi35 <hazminoname@gmail.com>
2018-10-25 22:22:32 +0700

    ok.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 var Sqlite = require('sqlite3');
-var _ = require('lodash'); //eslint-disable-line no-unused-vars
+var lodash = require('lodash'); //eslint-disable-line no-unused-vars
 module.exports = class DiscordEconomy {
     constructor() {
         var db = new Sqlite.Database('economy.sqlite');
         db.run("CREATE TABLE IF NOT EXISTS economy (id INTEGER, money INTEGER)");
-        this.fetchBalance = () => {
+        this.fetchBalance = (id) => { //eslint-disable-line no-unused-vars
             console.log('WIP');
         };
     }
