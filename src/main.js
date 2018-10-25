4bdadb0d0ded9805a04e475611ebec9a936a9ee8
Hazmi35 <hazminoname@gmail.com>
2018-10-25 22:02:17 +0700

    Commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-module.exports = {
-    
+var Sqlite = require('sqlite3');
+var _ = require('lodash');
+module.exports = class DiscordEconomy {
+    constructor() {
+        var db = new Sqlite.Database('economy.sqlite');
+        db.run("CREATE TABLE IF NOT EXISTS economy (id INTEGER, money INTEGER)");
+        this.fetchBalance = () => {
+            console.log('WIP');
+        }
+    }
 }
