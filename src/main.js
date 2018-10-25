711736a3c0b90539e3162a711e2b993fab66010c
Hazmi35 <hazminoname@gmail.com>
2018-10-25 22:16:14 +0700

    oops

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ module.exports = class DiscordEconomy {
         db.run("CREATE TABLE IF NOT EXISTS economy (id INTEGER, money INTEGER)");
         this.fetchBalance = () => {
             console.log('WIP');
-        }
+        };
     }
-}
+};
