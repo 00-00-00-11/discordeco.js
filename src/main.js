990b30a62f4e7de09bd8d83a72fc0ec935e25810
Hazmi35 <hazminoname@gmail.com>
2018-10-26 14:23:21 +0700

    development starting

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-var Sqlite = require('sqlite3');
+var Sqlite = require('sqlite3').verbose();
 var lodash = require('lodash'); //eslint-disable-line no-unused-vars
+const makeError = require('makeerror'); //eslint-disable-line
+const InputError = makeError('InputError'); //eslint-disable-line no-unused-vars
+
 module.exports = class DiscordEconomy {
     constructor() {
         var db = new Sqlite.Database('economy.sqlite');
-        db.run("CREATE TABLE IF NOT EXISTS economy (id INTEGER, money INTEGER)");
-        this.fetchBalance = (id) => { //eslint-disable-line no-unused-vars
-            console.log('WIP');
+        db.run("CREATE TABLE IF NOT EXISTS economy (userID TEXT, money INTEGER)");
+        this.fetchBalance = (ID) => { //eslint-disable-line no-unused-vars
+            if (!ID) return new InputError('Please input user ID to fetch the balance');
+            if (isNaN(ID)) return new InputError('Invalid ID');
         };
     }
 };
