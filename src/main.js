626a705a49cfe6b7c499a3feb3afa78d11a78c4f
Hazmi35 <hazminoname@gmail.com>
2018-10-27 22:44:20 +0700

    something

--- src/main.js
+++ src/main.js
@@ -4,12 +4,29 @@ const makeError = require('makeerror'); //eslint-disable-line
 const InputError = makeError('InputError'); //eslint-disable-line no-unused-vars
 
 module.exports = class DiscordEconomy {
-    constructor() {
+    constructor(opt) {
+        if (opt) {
+            var defaultBalance = lodash.toNumber(opt); //eslint-disable-line
+        } else {
+            var defaultBalance = 0; //eslint-disable-line
+        }
         var db = new Sqlite.Database('economy.sqlite');
         db.run("CREATE TABLE IF NOT EXISTS economy (userID TEXT, money INTEGER)");
-        this.fetchBalance = (ID) => { //eslint-disable-line no-unused-vars
-            if (!ID) return new InputError('Please input user ID to fetch the balance');
-            if (isNaN(ID)) return new InputError('Invalid ID');
+
+        //begin of functions
+
+        this.fetchBalance = function (ID) { //eslint-disable-line no-unused-vars
+                if (!ID) return new InputError('Please input user ID to fetch the balance');
+                if (isNaN(ID)) return new InputError('Invalid ID');
+                db.get(`SELECT * FROM economy WHERE userID = '${ID}'`, (err, row) => { //eslint-disable-line
+                    if (!row) {
+                        insertFirstMoney(ID); //eslint-disable-line
+                    } else {
+                        console.log(row.money); //TODO: Make this function return a money from row.
+                    }
+                });
         };
+
+        //end of functions
     }
 };
